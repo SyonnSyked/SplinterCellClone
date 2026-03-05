634b15542b18af905d9938dbd8a65569e43e4d6d
SyonnSyked <seanmgs2020@gmail.com>
2026-03-05 15:21:07 -0600

    GameManager and Basic Menu Setup
    GameManager and very basic UI with minimally functional buttons.

---
+++ SplinterCellClone/Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,35 @@
+using System.Globalization;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public void Resume()
+    {
+        GameManager.instance.StateUnpause();
+    }
+
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameManager.instance.StateUnpause();
+    }
+
+    public void Options()
+    {
+        GameManager.instance.OptionsScreen();
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#else
+        Application.Quit();
+#endif
+
+    }
+}
