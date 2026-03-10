e8dd2ec7ee6849c661345e2d2165862e56003b12
SyonnSyked <seanmgs2020@gmail.com>
2026-03-09 20:53:42 -0500

    Daily Task 3 50%

--- SplinterCellClone/Assets/Scripts/EnemyScripts/EnemyAI.cs
+++ SplinterCellClone/Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -1,30 +1,152 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour, iDamage
 {
 
     [Header("----Components----")]
     [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
 
     Color colorOriginal;
 
     [Header("----Stats----")]
     [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int FOV;
+    [SerializeField] int roamDist;
+    [SerializeField] int roamPauseTime;
+
+    [Header("----GunRelated----")]
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+    [SerializeField] Transform shootPos;
+    [SerializeField] Transform gun;
+
+
+
+    float shootTimer;
+    float roamTimer;
+    float angleToPlayer;
+    float stoppingDistanceOrig;
+
+    bool playerInTrigger;
+
+    Vector3 playerDir;
+    Vector3 startingPos;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         colorOriginal = model.material.color;
+        GameManager.instance.UpdateEnemyCount(1);
+        stoppingDistanceOrig = agent.stoppingDistance;
+        startingPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        shootTimer += Time.deltaTime;
+
+        if (agent.remainingDistance < 0.01f)
+            roamTimer += Time.deltaTime;
+
+        if (playerInTrigger && !CanSeePlayer())
+        {
+            CheckRoam();
+        }
+        else if (!playerInTrigger)
+        {
+            CheckRoam();
+        }
 
     }
 
+    void CheckRoam()
+    {
+        if (agent.remainingDistance < 0.01f && roamTimer >= roamPauseTime)
+        {
+            Roam();
+        }
+    }
+
+    void Roam()
+    {
+        roamTimer = 0;
+        agent.stoppingDistance = 0;
+
+        Vector3 randomPos = Random.insideUnitSphere * roamDist;
+        randomPos += startingPos;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomPos, out hit, roamDist, 1);
+        agent.SetDestination(hit.position);
+    }
+
+    void FaceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(playerDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    bool CanSeePlayer()
+    {
+        playerDir = GameManager.instance.player.transform.position - transform.position;
+        angleToPlayer = Vector3.Angle(playerDir, transform.forward);
+
+        Debug.DrawRay(shootPos.position, playerDir, Color.red);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(shootPos.position, playerDir, out hit))
+        {
+            if (angleToPlayer <= FOV && hit.collider.CompareTag("Player"))
+            {
+                agent.SetDestination(GameManager.instance.player.transform.position);
+
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    FaceTarget();
+                }
+
+                if (shootTimer > shootRate)
+                {
+                    Shoot();
+                }
+
+                agent.stoppingDistance = stoppingDistanceOrig;
+                return true;
+            }
+        }
+        agent.stoppingDistance = 0;
+        return false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+            agent.stoppingDistance = 0;
+        }
+    }
+
+    void Shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootPos.position, transform.rotation);
+    }
     public void TakeDamage(int amount)
     {
         HP -= amount;
