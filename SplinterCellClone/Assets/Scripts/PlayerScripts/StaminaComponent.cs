6aac5f7bd813604ad39d31d632f9e79bdbeb5538
SyonnSyked <seanmgs2020@gmail.com>
2026-03-11 15:21:40 -0500

    Stamina Basics
    Went ahead and added some basic stamina scaffolding. It drains properly, now it needs to recharge and then also disable sprinting or climbing when empty.

---
+++ SplinterCellClone/Assets/Scripts/PlayerScripts/StaminaComponent.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+public class StaminaComponent : MonoBehaviour
+{
+    [SerializeField] float staminaPool;
+    [SerializeField] float staminaLossRate;
+    [SerializeField] float staminaRechargeRate;
+    [SerializeField] float rechargeDelayTimer;
+
+    public float currentStamina;
+    float rechargeTimer;
+
+    bool staminaEmpty;
+    bool usingStamina;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentStamina = staminaPool;
+        rechargeTimer = rechargeDelayTimer;
+        UpdateStaminaMeter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (usingStamina)
+        {
+            DepleteStamina();
+            rechargeTimer = rechargeDelayTimer;
+        }
+        else
+        {
+            if (currentStamina < staminaPool)
+            {
+                rechargeTimer -= Time.deltaTime;
+
+                if (rechargeTimer <= 0)
+                {
+                    RechargeStamina();
+                }
+            }
+        }
+    }
+
+
+
+    void DepleteStamina()
+    {
+        currentStamina -= staminaLossRate * Time.deltaTime;
+
+        if (currentStamina <= 0)
+        {
+            currentStamina = 0;
+            staminaEmpty = true;
+        }
+    }
+
+    void RechargeStamina()
+    {
+        currentStamina += staminaRechargeRate * Time.deltaTime;
+
+        if (currentStamina >= staminaPool)
+        {
+            currentStamina = staminaPool;
+        }
+    }
+
+    void UpdateStaminaMeter()
+    { 
+
+    }
+
+
+    public void SetUsingStamina(bool state)
+    {
+        usingStamina = state;
+    }
+
+    public bool HasStamina()
+    {
+        return currentStamina > 0f;
+    }
+}
