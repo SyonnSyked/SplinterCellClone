65e70f28acf954857f1075584ccf720618d5c961
Jeremiah Richardson <JKRichardson1@student.fullsail.edu>
2026-03-14 11:14:56 -0400

    Added HitMarkerSystem script to display a hit indicator when the player damages an enemy.

---
+++ SplinterCellClone/Assets/Scripts/PlayerScripts/ParkourScripts/HitMarkerSystem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HitMarkerSystem : MonoBehaviour
+{
+
+    public static HitMarkerSystem instance;
+
+    [SerializeField] Image hitMarker;
+    [SerializeField] float hitMarkerTime = 0.2f;
+
+    float timer;
+
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            hitMarker.enabled = false;
+        }
+    }
+
+
+    public void ShowHitMarker()
+    {
+        hitMarker.enabled = true;
+        timer = hitMarkerTime;
+    }
+
+}
