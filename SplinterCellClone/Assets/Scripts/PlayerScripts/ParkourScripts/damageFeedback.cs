d51178fc9245005a85444f4a18161dc8d773ec20
Jeremiah Richardson <JKRichardson1@student.fullsail.edu>
2026-03-20 11:59:53 -0400

    added edge-based damage feedback using vignette-style overlay

---
+++ SplinterCellClone/Assets/Scripts/PlayerScripts/ParkourScripts/damageFeedback.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class damageFeedback : MonoBehaviour
+{
+
+    [SerializeField] Image damageImage;
+
+    [SerializeField] float fadeSpeed = 2f;
+    [SerializeField] float maxAlpha = 0.6f;
+
+    float currentAlpha;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentAlpha > 0)
+        {
+            currentAlpha -= fadeSpeed * Time.deltaTime;
+
+            if (currentAlpha < 0)
+                currentAlpha = 0;
+
+            Color color = damageImage.color;
+            color.a = currentAlpha;
+            damageImage.color = color;
+        }
+    }
+
+    //
+    public void showDamage(float amount)
+    {
+        currentAlpha += amount / 100f;
+
+        if (currentAlpha > maxAlpha)
+            currentAlpha = maxAlpha;
+
+        Color color = damageImage.color;
+        color.a = currentAlpha;
+        damageImage.color = color;
+    }
+
+}
